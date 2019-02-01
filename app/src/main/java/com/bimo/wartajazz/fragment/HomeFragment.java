675440aea274a7877257aba85251b671bdb67e87
Bimo Prakoso <bimprakosoo@gmail.com>
2019-02-01 21:38:29 +0700

    Add automatic slider

--- app/src/main/java/com/bimo/wartajazz/fragment/HomeFragment.java
+++ app/src/main/java/com/bimo/wartajazz/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.bimo.wartajazz.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,10 @@ public class HomeFragment extends Fragment {
 
     ViewPager viewPager;
     CustomSwipeAdapter adapter;
+    Handler h = new Handler();
+    int delay = 15000; //1 second
+    Runnable runnable;
+    private int[] pagerIndex = {-1};
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,6 +30,39 @@ public class HomeFragment extends Fragment {
         viewPager = (ViewPager) view.findViewById(R.id.view_pager);
         adapter = new CustomSwipeAdapter(this.getActivity());
         viewPager.setAdapter(adapter);
+        h.postDelayed(new Runnable() {
+                          public void run() {
+                              pagerIndex[0]++;
+                              if (pagerIndex[0] >= adapter.getCount()) {
+                                  pagerIndex[0] = 0;
+                              }
+
+                              viewPager.setCurrentItem(pagerIndex[0]);
+                              runnable=this;
+
+                              h.postDelayed(runnable, delay);
+                          }
+                      }
+                , delay);
         return view;
     }
+//
+//    public void onStart() {
+//        h.postDelayed(new Runnable() {
+//                          public void run() {
+//                              pagerIndex[0]++;
+//                              if (pagerIndex[0] >= adapter.getCount()) {
+//                                  pagerIndex[0] = 0;
+//                              }
+//
+//                              viewPager.setCurrentItem(pagerIndex[0]);
+//                              runnable=this;
+//
+//                              h.postDelayed(runnable, delay);
+//                          }
+//                      }
+//                , delay);
+//
+//        super.onStart();
+//    }
 }

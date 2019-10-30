0c34e4000b4b074a3117a672b884044cccdd939b
bimprakosoo <omib.ami@gmail.com>
2019-10-31 00:26:33 +0700

    Push notif

--- app/src/main/java/com/pkl/wartajazz/fragment/HomeFragment.java
+++ app/src/main/java/com/pkl/wartajazz/fragment/HomeFragment.java
@@ -13,13 +13,6 @@ import com.pkl.wartajazz.adapter.CustomSwipeAdapter;
 
 public class HomeFragment extends Fragment {
 
-    ViewPager viewPager;
-    CustomSwipeAdapter adapter;
-    Handler h = new Handler();
-    int delay = 15000; //1 second
-    Runnable runnable;
-    private int[] pagerIndex = {-1};
-
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -27,42 +20,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        viewPager = (ViewPager) view.findViewById(R.id.view_pager);
-        adapter = new CustomSwipeAdapter(this.getActivity());
-        viewPager.setAdapter(adapter);
-        h.postDelayed(new Runnable() {
-                          public void run() {
-                              pagerIndex[0]++;
-                              if (pagerIndex[0] >= adapter.getCount()) {
-                                  pagerIndex[0] = 0;
-                              }
-
-                              viewPager.setCurrentItem(pagerIndex[0]);
-                              runnable=this;
-
-                              h.postDelayed(runnable, delay);
-                          }
-                      }
-                , delay);
         return view;
     }
-//
-//    public void onStart() {
-//        h.postDelayed(new Runnable() {
-//                          public void run() {
-//                              pagerIndex[0]++;
-//                              if (pagerIndex[0] >= adapter.getCount()) {
-//                                  pagerIndex[0] = 0;
-//                              }
-//
-//                              viewPager.setCurrentItem(pagerIndex[0]);
-//                              runnable=this;
-//
-//                              h.postDelayed(runnable, delay);
-//                          }
-//                      }
-//                , delay);
-//
-//        super.onStart();
-//    }
 }
